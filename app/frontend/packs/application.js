51e059927b4462985b46e2b9aed140cc5a160403
Charles Pernet <charlespernet@gmail.com>
2018-10-25 15:37:09 +0200

    Install stimulus

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,6 +1,13 @@
 import Turbolinks from 'turbolinks';
 import 'style';
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 // Turbolinks
 Turbolinks.start();
 
