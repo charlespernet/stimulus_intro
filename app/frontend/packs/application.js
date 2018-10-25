1f7e813541b5d3328806f2042ef36a25952c62ff
Charles Pernet <charlespernet@gmail.com>
2018-10-25 14:47:58 +0200

    Cleaner starting point

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,10 +1,7 @@
 import Turbolinks from 'turbolinks';
 import 'style';
-import { clicker } from '../clicker';
 
 // Turbolinks
 Turbolinks.start();
 
-document.addEventListener("turbolinks:load", () => {
-  clicker.initialize()
-})
+console.log('Started!')
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'carts#show'
-  resources :carts, only: :show do
-    resources :items, only: :create
-  end
+  root to: 'pages#intro'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
