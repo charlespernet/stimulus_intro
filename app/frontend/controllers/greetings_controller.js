e670bbc1c622a5d14e13e51e48399493d9d4b117
Charles Pernet <charlespernet@gmail.com>
2018-10-25 16:14:00 +0200

    Connect and action

--- app/frontend/controllers/greetings_controller.js
+++ app/frontend/controllers/greetings_controller.js
@@ -1,7 +1,16 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = ['message', 'input']
+
   connect() {
     console.log("Hello, Stimulus!", this.element)
+  }
+
+  greet() {
+    console.log('Oh you want the greet ?')
+    const name = this.inputTarget.value || 'BoGossDu91'
+    this.messageTarget.innerText = `"Welcome ${name}"`
+    this.messageTarget.classList.add('message--revealed')
   }
 }
